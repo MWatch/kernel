0225ed4d81f6de7f5cd88b0c008b16a5f2639649
Scott <MabezDev@gmail.com>
2018-01-27 12:08:28 +0000

    [MWR(MabezWatchRust)] Working toolchain/workflow for stm32f103xx, printf via semihosting, will try to get itm working

---
+++ examples/allocator.rs
@@ -0,0 +1,79 @@
+//! How to use the heap and a dynamic memory allocator
+//!
+//! To compile this example you'll need to build the collections crate as part
+//! of the Xargo sysroot. To do that change the Xargo.toml file to look like
+//! this:
+//!
+//! ``` text
+//! [dependencies.core]
+//! stage = 0
+//!
+//! [dependencies.collections] # NEW
+//! stage = 0
+//!
+//! [dependencies.compiler_builtins]
+//! stage = 1
+//! ```
+//!
+//! This example depends on the alloc-cortex-m crate so you'll have to add it
+//! to your Cargo.toml:
+//!
+//! ``` text
+//! # or edit the Cargo.toml file manually
+//! $ cargo add alloc-cortex-m
+//! ```
+//!
+//! ---
+
+#[allow(deprecated)]
+#![feature(collections)]
+#![feature(used)]
+#![no_std]
+
+// This is the allocator crate; you can use a different one
+extern crate alloc_cortex_m;
+#[macro_use]
+extern crate collections;
+extern crate cortex_m;
+extern crate cortex_m_rt;
+extern crate cortex_m_semihosting;
+
+use core::fmt::Write;
+
+use cortex_m::asm;
+use cortex_m_semihosting::hio;
+
+fn main() {
+    // Initialize the allocator
+    unsafe {
+        extern "C" {
+            // Start of the heap
+            static mut _sheap: usize;
+        }
+
+        // Size of the heap in words (1 word = 4 bytes)
+        // NOTE The bigger the heap the greater the chance to run into a stack
+        // overflow (collision between the stack and the heap)
+        const SIZE: isize = 256;
+
+        // End of the heap
+        let _eheap = (&mut _sheap as *mut _).offset(SIZE);
+
+        alloc_cortex_m::init(&mut _sheap, _eheap);
+    }
+
+    // Growable array allocated on the heap
+    let xs = vec![0, 1, 2];
+
+    let mut stdout = hio::hstdout().unwrap();
+    writeln!(stdout, "{:?}", xs).unwrap();
+}
+
+// As we are not using interrupts, we just register a dummy catch all handler
+#[link_section = ".vector_table.interrupts"]
+#[used]
+static INTERRUPTS: [extern "C" fn(); 240] = [default_handler; 240];
+
+extern "C" fn default_handler() {
+    asm::bkpt();
+}
---
+++ examples/itm.rs
@@ -0,0 +1,40 @@
+//! Sends "Hello, world!" through the ITM port 0
+//!
+//! **IMPORTANT** Not all Cortex-M chips support ITM. You'll have to connect the
+//! microcontroller's SWO pin to the SWD interface. Note that some development
+//! boards don't provide this option.
+//!
+//! ITM is much faster than semihosting. Like 4 orders of magnitude or so.
+//!
+//! You'll need [`itmdump`] to receive the message on the host plus you'll need
+//! to uncomment the `monitor` commands in the `.gdbinit` file.
+//!
+//! [`itmdump`]: https://docs.rs/itm/0.1.1/itm/
+//!
+//! ---
+
+#![feature(used)]
+#![no_std]
+
+#[macro_use]
+extern crate cortex_m;
+extern crate cortex_m_rt;
+
+use cortex_m::{asm, interrupt, peripheral};
+
+fn main() {
+    interrupt::free(|cs| {
+        let itm = peripheral::ITM.borrow(&cs);
+
+        iprintln!(&itm.stim[0], "Hello, world!");
+    });
+}
+
+// As we are not using interrupts, we just register a dummy catch all handler
+#[link_section = ".vector_table.interrupts"]
+#[used]
+static INTERRUPTS: [extern "C" fn(); 240] = [default_handler; 240];
+
+extern "C" fn default_handler() {
+    asm::bkpt();
+}
