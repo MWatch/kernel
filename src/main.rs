dcceb3d96c7a62bf578370bb7d18344c22245ca2
Scott <MabezDev@gmail.com>
2018-01-28 15:17:58 +0000

    [MWR:Serial]
    	- Added STM32F103 HAL from git using cargo add {git url}
    	- Using the HAL, added a serial echoer. Still needs testing
    	(Requires building the MabezWatch APK)

--- src/main.rs
+++ src/main.rs
@@ -8,16 +8,60 @@
 extern crate cortex_m;
 extern crate cortex_m_rt;
 extern crate cortex_m_semihosting;
+extern crate stm32f103xx_hal as hal;
+#[macro_use(block)]
+extern crate nb;
 
 use core::fmt::Write;
-
-use cortex_m::asm;
+use cortex_m::{asm};
 use cortex_m_semihosting::hio;
+use hal::prelude::*;
+use hal::serial::Serial;
+use hal::stm32f103xx;
+
 
 fn main() {
     let mut stdout = hio::hstdout().unwrap();
+    let p = stm32f103xx::Peripherals::take().unwrap();
+
+    writeln!(stdout, "Hello, world!").unwrap();
+
+    /* Borrow peripherals, flash and rcc(clocks) */
+    let mut flash = p.FLASH.constrain();
+    let mut rcc = p.RCC.constrain();
+
+    /* Lock clocks */
+    let clocks = rcc.cfgr.freeze(&mut flash.acr);
+
+    /* Alternate function IO (SERIAL/SPI/I2C etc) */
+    let mut afio = p.AFIO.constrain(&mut rcc.apb2);
+
+    let mut gpioa = p.GPIOA.split(&mut rcc.apb2);
+    // let mut gpiob = p.GPIOB.split(&mut rcc.apb2);
+
+    // USART1 pins
+    let tx = gpioa.pa9.into_alternate_push_pull(&mut gpioa.crh);
+    let rx = gpioa.pa10;
+
+    /* Initialze UART1 */
+    let serial = Serial::usart1(
+        p.USART1,   /* Peripheral */
+        (tx, rx),   /* Pin Tuple */
+        &mut afio.mapr,
+        9_600.bps(),    /* Baud rate */
+        clocks,         /*  Periph clock speed */
+        &mut rcc.apb2,
+    );
+
+    let (mut tx, mut rx) = serial.split();
+
+    let sent = b'Y';
     loop {
-        writeln!(stdout, "Hello, world!").unwrap();
+        writeln!(stdout, "Transmitting : {}", sent).unwrap();
+        block!(tx.write(sent)).ok();
+        writeln!(stdout, "Waiting on resp").unwrap();
+        let received = block!(rx.read()).unwrap();
+        writeln!(stdout, "We recieved {}", received).unwrap();
     }
 }
 
