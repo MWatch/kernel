305dee225619afac1abb8819c2faa05e971c7ff9
Scott Mabin <scott@mabez.dev>
2022-12-29 23:28:16 +0000

    asyncify esp32s3

--- kernel-esp32s3/src/main.rs
+++ kernel-esp32s3/src/main.rs
@@ -1,14 +1,26 @@
 #![no_std]
 #![no_main]
+#![feature(type_alias_impl_trait)]
 
-use esp32s3_hal::{clock::ClockControl, pac::Peripherals, prelude::*, timer::TimerGroup, Rtc};
+use embassy_executor::Executor;
+use embassy_time::{Timer, Duration};
+use esp32s3_hal::{clock::ClockControl, pac::Peripherals, prelude::*, timer::TimerGroup, Rtc, embassy};
 use esp_backtrace as _;
+use esp_println;
+use static_cell::StaticCell;
+
+static EXECUTOR: StaticCell<Executor> = StaticCell::new();
 
 #[xtensa_lx_rt::entry]
 fn main() -> ! {
     let peripherals = Peripherals::take().unwrap();
     let system = peripherals.SYSTEM.split();
     let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
+
+    esp_println::logger::init_logger(log::LevelFilter::Info);
+
+    log::info!("\r\n\r\n  /\\/\\/ / /\\ \\ \\__ _| |_ ___| |__  \r\n /    \\ \\/  \\/ / _` | __/ __| '_ \\ \r\n/ /\\/\\ \\  /\\  / (_| | || (__| | | |\r\n\\/    \\/\\/  \\/ \\__,_|\\__\\___|_| |_|\r\n                                   \r\n");
+    log::info!("Copyright Scott Mabin 2022");
 
     // Disable the RTC and TIMG watchdog timers
     let mut rtc = Rtc::new(peripherals.RTC_CNTL);
@@ -21,5 +33,21 @@ fn main() -> ! {
     wdt0.disable();
     wdt1.disable();
 
-    loop {}
+    embassy::init(
+        &clocks,
+        esp32s3_hal::systimer::SystemTimer::new(peripherals.SYSTIMER),
+    );
+
+    let executor = EXECUTOR.init(Executor::new());
+    executor.run(|spawner| {
+        spawner.spawn(idle()).ok();
+    })
+}
+
+#[embassy_executor::task]
+async fn idle() {
+    loop {
+        log::info!("Bing!");
+        Timer::after(Duration::from_millis(3_000)).await;
+    }
 }
